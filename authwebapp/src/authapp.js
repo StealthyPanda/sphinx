b2cb537a6bdc15186938158678de515ce25c03cf
StealthyPanda <shaikm259@gmail.com>
2023-05-22 02:19:39 +0400

    Ready for dev deploy

--- authwebapp/src/authapp.js
+++ authwebapp/src/authapp.js
@@ -1,25 +1,52 @@
 import './authapp.css'
 import image from './logo192.png';
+import {useState} from 'react';
 
-export const Textbox = () => {
 
-    return (
-        <input type = "text" className='textbox'>
-        </input>
-    );
-
+const getjson = sp => {
+    let obj = {};
+    sp.forEach((value, key) => {
+      obj[key] = value;
+    });
+    return obj;
 };
+const searchParams = new URLSearchParams(document.location.search);
+const queryjson = getjson(searchParams);
+
+
 
 export const Authform = () => {
+    const [user, changeUser] = useState('');
+    const [pass, changePass] = useState('');
+    
+    const formhandler = () => {
+        if ((!user) || (!pass)) return;
 
+        const finalobj = {
+            'uid' : user,
+            'pass' : pass,
+            ...queryjson
+        };
+        console.log(finalobj);
+    };
 
     return (
         <div className='authform'>
-            <img src={image} alt='placeholder' className='placeholder'/>
+            <img src={image} alt='placeholder' className='placeholder' />
             <h1>Sphinx Auth</h1>
-            <input className='textbox' type='text' placeholder='username'></input>
-            <input className='textbox' type='password' placeholder='password'></input>
-            <button>Login/Register</button>
+            <input className='textbox' type='text' placeholder='username' onChange={
+                event => {
+                    changeUser(event.target.value);
+                    // console.log(pass);
+                }
+            }></input>
+            <input className='textbox' type='password' placeholder='password' onChange={
+                event => {
+                    changePass(event.target.value);
+                    // console.log(pass);
+                }
+            }></input>
+            <button onClick={formhandler}>Login/Register</button>
         </div>
     );
 };
